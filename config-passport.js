044bcbef1cf1c06a96e7e389e00f8d3ec083deb7
leephu93 <lee.phu93@gmail.com>
2018-11-07 23:02:57 +0700

    Xóa config-passport.js

--- config-passport.js
+++
@@ -1,8 +0,0 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const GitHubStrategy = require('passport-github').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const config = require('./config');
-const util = require('./util');
-
